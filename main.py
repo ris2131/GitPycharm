406781c206e9293d7b6c298a8e809152bed9b64e
ris2131 <49578522+ris2131@users.noreply.github.com>
2020-10-04 20:38:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,17 @@
 print("Hello world")
 
 
-#adding new item in hotfix
+#adding new item from hotfix
 print("HEllO WORLD2")
 
 #adding new item from master
 print("HEllO WORLD3")
 
-#adding new item in hotfix
+#adding new item from hotfix
 print("HEllO WORLD4")
 
-#adding new item in hotfix
+#adding new item from master
 print("HEllO WORLD5")
+
+#adding new item from gitgub
+print("HEllO WORLD update on github")
