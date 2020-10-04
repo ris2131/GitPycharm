57e71b7467a15a0b7bde2692948355e3a37c8936
ris2131 <mstoto23@naver.com>
2020-10-04 20:23:56 +0900

    helloworld5 on Master branch

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ print("HEllO WORLD2")
 #adding new item from master
 print("HEllO WORLD3")
 
-
-
 #adding new item in hotfix
 print("HEllO WORLD4")
+
+#adding new item in hotfix
+print("HEllO WORLD5")
